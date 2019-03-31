b2959f511abbcd9718aa8860bd975a8518b0611f
Juan López Rubio <j.lopez.r@gmail.com>
2019-03-31 18:52:42 +0200

    test json equality with lodash

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,3 +1,4 @@
+import * as _ from "lodash";
 import * as request from "supertest";
 import app = require('../app');
 
@@ -8,5 +9,16 @@ describe("Test the root path", () => {
 
     expect(response.status).toBe(200);
     expect(response.text).toBe("Hello world!");
+  });
+});
+
+describe("Test the /hello path", () => {
+  test("It should respond to the GET method", async () => {
+    
+    const response = await request(app).get("/hello");
+
+    expect(response.status).toBe(200);
+    expect(response.type).toEqual("application/json");
+    expect(_.isEqual(JSON.parse(response.text),{"hello":"world"})).toBe(true);
   });
 });
